f8295cb39a24ea041269768b2423abbeb8857f92
Poushali Chowdhury <poushalichowdhury80@gmail.com>
2024-01-06 23:18:08 +0530

    install parcel

--- App.js
+++ App.js
@@ -17,6 +17,8 @@
         </div>
     </div>
  */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child1' }, [
